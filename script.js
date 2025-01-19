e63072a45ce64d86b0ce972f14fd94e94333bed1
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-01-19 13:18:42 +0400

    adding/JS

---
+++ script.js
@@ -0,0 +1,61 @@
+// პირველი---------------------------task--1--
+let age_controle = prompt("რამდენი წლის ხარ?");
+if (age_controle <= 12) {
+  console.log("თქვენ არ გაქვთ შესვლის უფლება.");
+} else if (age_controle > 12) {
+  console.log("თქვენ შეგიძლიათ დაესწროთ ფილმს.");
+}
+// მეორე---------------------------task--2--
+// let Traffic_light = prompt("რომელი ფერია შუქნიშანზე?");
+// if (Traffic_light === "ყვითელი") {
+//   console.log("მოემზადე");
+// } else if (Traffic_light === "წითელი") {
+//   console.log("გაჩერდი");
+// } else if (Traffic_light === "მწვანე") {
+//   console.log("შეგიძლია წახვიდე");
+// }
+// მესამე---------------------------task--3--
+// let clock = prompt("რომელი საათია?");
+// if (clock < 12) {
+//   console.log("დილა მშვიდობის");
+// } else if (clock < 18) {
+//   console.log("შუადღე მშვიდობის");
+// } else {
+//   console.log("საღამო მშვიდობის");
+// }
+// მეოთხე-------------------------task--4--
+// let exam = prompt("ჩაწერეთ თქვენი ქულა");
+// if (exam >= 90) {
+//   console.log("A");
+// } else if (exam >= 70) {
+//   console.log("B");
+// } else if (exam >= 50) {
+//   console.log("C");
+// } else {
+//   console.log("F");
+// }
+// მეხუთე-------------------------task--5-------bonus-----
+// let ageControle = Number(prompt("Enter your age:"));
+// let movieType = prompt("Enter movie type (regular or 3D):");
+// let Part_of_the_day = prompt("Enter time of day (matinee or evening):");
+// let totalPrice = Number(0);
+// if (ageControle <= 10) {
+//   totalPrice = totalPrice + 5;
+// } else if (ageControle >= 65) {
+//   totalPrice = totalPrice + 10;
+// } else {
+//   totalPrice = totalPrice + 15;
+// }
+// if (movieType === "3D") {
+//   totalPrice = totalPrice + 5;
+// } else {
+//   totalPrice = totalPrice + 0;
+// }
+// if (Part_of_the_day === "evening") {
+//   totalPrice = totalPrice + 5;
+// } else {
+//   totalPrice = totalPrice + 0;
+// }
+// console.log(`ბილეთის ფასი თქვენთვის არის: ${totalPrice} ლარი.
+// `);
+// END-------------------------END-------------------------END
